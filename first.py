e620269fe615ff283287851be8d6688756a838f0
nithindev01 <nithindev.v20@iiits.in>
2022-01-28 21:24:20 +0530

    Vanga Nithin Dev

---
+++ first.py
@@ -0,0 +1,21 @@
+print("Hello world")
+if 5 > 2:
+    print("five greater than two!")
+    print("five greater than two!")
+
+x = 8
+x="home"
+print("The resut is " + x)
+a=1002312
+b=12273
+print(a+b)
+
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
